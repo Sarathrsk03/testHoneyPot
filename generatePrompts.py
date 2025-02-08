ff97ad4f2af5c75a5da472dcb27ff88aaafd5566
Sarath Rajan Senthilkumar <118745490+Sarathrsk03@users.noreply.github.com>
2025-02-08 22:31:09 +0530

    Update generatePrompts.py

--- generatePrompts.py
+++ generatePrompts.py
@@ -5,6 +5,13 @@ from pptCreate import generatePPTFromDict
 from xlsxCreate import createXLSXFromDict
 from time import sleep
 
+import os 
+industry = os.getenv("industtry") 
+companyName = os.getenv("companyName") 
+role = os.getenv("role")
+
+desktop = os.path.join(os.path.expanduser("~"), "Desktop")
+
 def createPrompts(companyName: str, industry: str, role: str) -> list:
     prompt = f"Company Name: {companyName}\nIndustry: {industry}\nRole: {role}\n\n"
     
@@ -25,6 +32,8 @@ if __name__ == "__main__":
         fileName = filePrompt["file_name"]
         fileType = filePrompt["file_type"]
         contentPrompt = filePrompt["prompt"]
+
+        fileName = os.path.join(desktop,fileName)
         
         print(f"File Name: {fileName}\nFile Type: {fileType}\nContent Prompt: {contentPrompt}\n\n")
 
