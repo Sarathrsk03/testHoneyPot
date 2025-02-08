094a1d9c1529c3c1183718b5f55e0d0fd6bdecaf
Sarath Rajan Senthilkumar <118745490+Sarathrsk03@users.noreply.github.com>
2025-02-09 00:03:12 +0530

    Update setupHoneypot.sh

--- setupHoneypot.sh
+++ setupHoneypot.sh
@@ -1,26 +1,33 @@
 # !/bin/bash 
 
-sudo apt-get install -y python3
-sudo apt-get install -y python3-pip 
+sudo apt-get install -y python3-all 
 sudo apt-get install -y git 
 
-git pull https://github.com/Sarathrsk03/testHoneyPot test 
+git clone https://github.com/Sarathrsk03/testHoneyPot
 
 cd testHoneyPot 
+
+python3 -m venv venv 
+
+source venv/bin/activate
+
+pip install -r requirements.txt
+
 
 read -p "Enter the company name: " companyNameInp 
 read -p "Enter the industry: " industryInp
 read -p "Enter the role: " roleInp
-export companyName= $companyNameInp
-export industry= $industryInp
-export role= $roleInp
+export companyName=$companyNameInp
+export industry=$industryInp
+export role=$roleInp
 
-#export geminiAPI=
+export geminiAPI=
 
-python3 generatePrompts.py 
+python3 docxCreate.py 
+python3 pptCreate.py
 
 echo "All tasks completed"
 cd .. 
 rm setup.sh
-rm -rf testHoneypot
+rm -rf testHoneyPot
 exit
