e8bbb206b2eacca59b6edfa1e2e47621e91e5f7f
Sarath Rajan Senthilkumar <118745490+Sarathrsk03@users.noreply.github.com>
2025-02-18 04:14:41 +0530

    Update sqlGeneratePrompts.py

--- sqlGeneratePrompts.py
+++ sqlGeneratePrompts.py
@@ -1,13 +1,18 @@
 from sql import createTableAndInsertData
 from llmConnect import generateSQLPrompts
 from json import loads
+
+import os 
+industry = os.getenv("industtry") 
+companyName = os.getenv("companyName") 
+
 def createSQlPrompts(companyName:str, industry:str):
     prompt = f"Company name: {companyName}, Industry: {industry}"
     return prompt
 
 if __name__ == "__main__":
     try:
-        prompt = createSQlPrompts("Divij Industries","Textiles")
+        prompt = createSQlPrompts(companyName,industry)
         prompts = loads(generateSQLPrompts(prompt))
         #print(prompts)
         for i in prompts:
