2f28bfbd9e5c45611524add22939b69702dad46e
Sarath Rajan Senthilkumar <118745490+Sarathrsk03@users.noreply.github.com>
2025-02-18 16:50:24 +0530

    Update sqlGeneratePrompts.py

--- sqlGeneratePrompts.py
+++ sqlGeneratePrompts.py
@@ -16,9 +16,13 @@ if __name__ == "__main__":
         prompts = loads(generateSQLPrompts(prompt))
         #print(prompts)
         for i in prompts:
-            print(i,"\n")
-            createTableAndInsertData(i)
-            print("-"*100,"\n")
+            try: 
+                print(i,"\n")
+                createTableAndInsertData(i)
+                print("-"*100,"\n")
+            except Exception as e:
+                print(e)
+                continue
     except Exception as e:
         print("Hit an exception")
         print(e)
